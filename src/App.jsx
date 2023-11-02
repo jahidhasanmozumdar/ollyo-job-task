9f6fa2e6898a1aa41f130721898fee6d9c9b5d03
jahidhasanmozumdar <jahidhasanmozumdar9@gmail.com>
2023-11-02 22:07:41 +0600

    deploy issue solve

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import "./App.css";
 
 function App() {
   const images = [
-    { id: 1, src: "../src/assets/image-1.webp" },
-    { id: 2, src: "../src/assets/image-2.webp" },
-    { id: 3, src: "../src/assets/image-3.webp" },
-    { id: 4, src: "../src/assets/image-4.webp" },
-    { id: 5, src: "../src/assets/image-5.webp" },
-    { id: 6, src: "../src/assets/image-6.webp" },
-    { id: 7, src: "../src/assets/image-7.webp" },
-    { id: 8, src: "../src/assets/image-8.webp" },
-    { id: 9, src: "../src/assets/image-9.webp" },
-    { id: 10, src: "../src/assets/image-10.jpeg" },
-    { id: 11, src: "../src/assets/image-11.jpeg" },
+    { id: 1, src: "./image-1.webp" },
+    { id: 2, src: "./image-2.webp" },
+    { id: 3, src: "./image-3.webp" },
+    { id: 4, src: "./image-4.webp" },
+    { id: 5, src: "./image-5.webp" },
+    { id: 6, src: "./image-6.webp" },
+    { id: 7, src: "./image-7.webp" },
+    { id: 8, src: "./image-8.webp" },
+    { id: 9, src: "./image-9.webp" },
+    { id: 10, src: "./image-10.jpeg" },
+    { id: 11, src: "./image-11.jpeg" },
   ];
   const [imageList, setImageList] = useState(images);
   const [selectedImages, setSelectedImages] = useState([]);
